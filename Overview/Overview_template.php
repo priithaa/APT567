648544f54855d821faaaf7929fc3e59a16c7cc0d
ananya0504 <ananyagupta0504@gmail.com>
2021-03-01 23:23:19 +0530

    Added Overview files

---
+++ Overview/Overview_template.php
@@ -0,0 +1,87 @@
+<?php require_once 'menu_template.php' ?>
+
+<div class="col-sm-9">
+        <h5 class="p-4">
+          Course Code: CS302 <br>
+          Course Name: Data Communication and Networks<br>
+          Contact Hours/Week: 4 <br>
+          Credit Points: 4<br>
+          Course Instructors: Dr. Monika Saxena<br>
+          Evaluation Scheme:
+          <ul>
+            <li>
+              Continuous Assessment: 40 marks
+            </li>
+            <li>
+              End Semester: 60 marks 
+            </li>
+          </ul>
+        </h5>
+      
+        <p class="px-4">
+          <h5 class="p-4">Learning Outcomes:</h5>
+          <ul>
+            After successful completion of the course students will be able to
+            <li>
+              Understand basic computer network technology and the Data Communications System and its components.
+            </li>
+            <li>
+              Describe the layers of the OSI model and TCP/IP and the function(s) of each layer.
+            </li>
+            <li>
+              Describe the importance of data communications and the Internet in supporting business communications and
+              daily activity. 
+            </li>
+            <li>
+              Analyze the features and working of IPV4, IPV6 and their transition with Connection less and Connection  
+              oriented Transport layer protocols (TCP/UDP).
+            </li>
+            <li>
+              Analyze the features and operations of various protocols such as HTTP, DNS, SMTP and many more 
+              application  layer protocols.
+            </li>
+          </ul>
+        </p>
+
+        <p class="px-4">
+          <h5 class="p-4"> Suggested Readings:</h5>
+          <ol>
+            <li>
+              Stallings, W. (2007). Data and computer communications. Pearson Education India.
+            </li>
+            <li>
+              Forouzan, A. B. (2007). Data communications & networking. Tata McGraw-Hill Education.
+            </li>
+            <li>
+              Tanenbaum, A. S. (2014). Computer networks. PHI. 
+            </li>
+            <li>
+              Kurose, J. F., & Ross, K. W. (2009). Computer networking: a top-down approach. 
+              Pearson Education. 
+            </li>
+            <li>
+              Gupta, P. C. (2013). Data communications and computer networks. 
+              PHI Learning Pvt. Ltd. 
+            </li>
+            <li>
+              Couch, I. I., & Leon, W. (1998). 
+              Modern Communication Systems: Principles and Applications. PHI  Learning. 
+            </li>
+          </ol>
+        </p>
+
+        <p class="px-4">
+          <h5 class="p-4">Suggested E-Resources:</h5>
+          <ul>
+            <li>
+              Computer Networking: A Top-Down Approach by James F. Kurose and Keith W. Ross
+              <a href="https://www.bau.edu.jo/UserPortal/UserProfile/PostsAttach/10617_1870_1.pdf"> Click here </a>
+            </li>
+            <li>
+              Data Communication: <a href="https://nptel.ac.in/courses/106105082/">Click here</a>
+            </li>
+          </ul>
+        </p>
+      </div>
+
+    <?php require_once 'twitter_template.php' ?>
