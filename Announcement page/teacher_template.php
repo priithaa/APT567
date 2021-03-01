36adea76602f1c113c280e5ebdf69630ad514493
ananya0504 <ananyagupta0504@gmail.com>
2021-03-01 17:10:32 +0530

    Added Announcement files in PHP

---
+++ Announcement page/teacher_template.php
@@ -0,0 +1,109 @@
+<?php require_once 'menu_template.php' ?>
+
+<div class="col-sm-9">
+        <div class="p-4 row">
+            <div class="row Ann_add">
+              <button>
+                <i class='bx bx-plus'></i>
+                <span class="mx-2"> Add Announcement</span>
+              </button>
+            </div>
+            <div class="whitebox">
+                <div class ="contentbox">
+                    <div class="row Ann_head">
+                        <div class="col sm Ann_title">
+                            <h5>
+                                <a href="">Quiz on 29th Feb 2021 </a>
+                            </h5>
+                        </div>
+                        <div class="col sm Ann-date">
+                            <h6>Posted: 21/02/2021</h6>
+                        </div>
+                    </div>
+                    <div class="row Ann_desc">
+                        An article is any member of a class of dedicated words that are 
+                        used with noun phrases to mark the identifiability of the referents of 
+                        the noun phrases. The category of articles constitutes a part of speech. 
+                        In English, both "the" and "a" are articles, which combine with a noun to 
+                        form a noun phrase. Wikipedia
+                    </div>
+                    <div class="row Ann_delete">
+                      <button>Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="whitebox">
+                <div class ="contentbox">
+                    <div class="row Ann_head">
+                        <div class="col sm Ann_title">
+                            <h5>
+                                <a href="">Quiz on 29th Feb 2021 </a>
+                            </h5>
+                        </div>
+                        <div class="col sm Ann-date">
+                            <h6>Posted: 21/02/2021</h6>
+                        </div>
+                    </div>
+                    <div class="row Ann_desc">
+                        An article is any member of a class of dedicated words that are 
+                        used with noun phrases to mark the identifiability of the referents of 
+                        the noun phrases. 
+                    </div>
+                    <div class="row Ann_delete">
+                      <button>Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="whitebox">
+                <div class ="contentbox">
+                    <div class="row Ann_head">
+                        <div class="col sm Ann_title">
+                            <h5>
+                                <a href="">Quiz on 29th Feb 2021 </a>
+                            </h5>
+                        </div>
+                        <div class="col sm Ann-date">
+                            <h6>Posted: 21/02/2021</h6>
+                        </div>
+                    </div>
+                    <div class="row Ann_desc">
+                        The category of articles constitutes a part of speech. 
+                        In English, both "the" and "a" are articles, which combine with a noun to 
+                        form a noun phrase.
+                    </div>
+                    <div class="row Ann_delete">
+                      <button>Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="whitebox">
+                <div class ="contentbox">
+                    <div class="row Ann_head">
+                        <div class="col sm Ann_title">
+                            <h5>
+                                <a href="">Quiz on 29th Feb 2021 </a>
+                            </h5>
+                        </div>
+                        <div class="col sm Ann-date">
+                            <h6>Posted: 21/02/2021</h6>
+                        </div>
+                    </div>
+                    <div class="row Ann_desc">
+                        The category of articles constitutes a part of speech. 
+                        In English, both "the" and "a" are articles, which combine with a noun to 
+                        form a noun phrase. The category of articles constitutes a part of speech. 
+                        In English, both "the" and "a" are articles, which combine with a noun to 
+                        form a noun phrase.
+                    </div>
+                    <div class="row Ann_delete">
+                      <button>Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+
+      <?php require_once 'twitter_template.php' ?>
---
+++ Announcement page/twitter_template.php
@@ -0,0 +1,17 @@
+                <div class="col-sm-3" style="background-color: #ffff;">
+                    <a class="twitter-timeline"
+                            data-width="400" 
+                            data-height="700" 
+                            data-theme="light" 
+                            href="https://twitter.com/Banasthali_Vid?ref_src=twsrc%5Etfw" > Tweets by Banasthali_Vid
+                    </a> 
+                    
+                    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8">
+                    </script>
+                    
+                </div>
+
+            </div>  
+        </div>
+    </body>
+</html>
