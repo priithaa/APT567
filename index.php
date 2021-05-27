127ee256431795774ae4a17c28690800071f5a7e
ananya0504 <ananyagupta0504@gmail.com>
2021-05-28 01:50:33 +0530

    Added index.php which our landing page

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+
+    <head >
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+
+    <body style='text-align:center;' >
+
+        <div id="container">
+        <div id="content">
+            <div id="about">
+            <h1>
+                <div>
+                WELCOME TO
+                </div>
+                <div style='margin-bottom:20px;'>
+                BV CLASSROOM
+                </div>
+                <!-- <div class="stage">
+                <div class="pyramid3d">
+                    <div class="triangle side1"></div>
+                    <div class="triangle side2"></div>
+                    <div class="triangle side3"></div>
+                    <div class="triangle side4"></div>
+                </div>
+                </div>  -->
+                
+                <!-- <button type="button" class="btn btn-lg">
+					<a href="Login/login.php">
+						Getting Started
+					</a>
+				</button> -->
+            </h1>
+            <h3 >
+                <a href="Login/login.php", style='color:black'>Getting Started</a>
+            </h3>
+            
+            <p>
+            Our project is an Academic Portal which will help the universities and the schools to conduct 
+            online education efficiently. It will help teachers and students have their courses, and the 
+            required material at one place.
+            We will  give the facility to teachers to add the announcements, assignments, grades and 
+            resources of respective courses and students with the facility to access the same. Students 
+            can also submit their answer sheets on the portal. Furthermore, a to-do list feature is 
+            provided for the students to help keep a check on their due assignments.
+            </p>
+            
+            </div>
+        </div>
+        </div>
+
+    </body>
+</html>
